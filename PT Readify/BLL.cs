4e350c24b44ac3199c635cfc1c07ccce9e477e79
pabloxerequices <digorodry826@gmail.com>
2026-03-02 16:47:34 +0000

    Adicionar arquivos de projeto.

---
+++ PT Readify/BLL.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DataAccessLayer;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace BusinessLogicLayer
+{
+    public class BLL
+    {
+        
+        public class Clientes
+        {
+
+            static public DataTable Load()
+            {
+                DAL dal = new DAL();
+                return dal.executarReader("select * from Clientes", null);
+            }
+            static public int insertCliente(string nome, string morada, string telefone)
+            {
+                DAL dal = new DAL();
+                SqlParameter[] sqlParams = new SqlParameter[]{
+                new SqlParameter("@nome", nome),
+                new SqlParameter("@morada", morada),
+                new SqlParameter("@telefone", telefone)
+				};
+
+                return dal.executarNonQuery("INSERT into Clientes (Nome,Morada,Telefone) VALUES(@nome,@morada,@telefone)", sqlParams);
+            }
+            static public DataTable queryClienteLike(String nome)
+            {
+                DAL dal = new DAL();
+                SqlParameter[] sqlParams = new SqlParameter[]{
+                new SqlParameter("@nome", nome + "%")
+                };
+                return dal.executarReader("select * from Clientes where Nome like @nome", sqlParams);
+            }
+            static public DataTable queryClientePorId(int id)
+            {
+                DAL dal = new DAL();
+                SqlParameter[] sqlParams = new SqlParameter[]{
+                new SqlParameter("@id", id)
+                };
+                return dal.executarReader("select * from Clientes where ID=@id", sqlParams);
+            }
+            static public DataTable queryClientePorIdENome(int id, string nome)
+            {
+                DAL dal = new DAL();
+                SqlParameter[] sqlParams = new SqlParameter[]{
+                new SqlParameter("@id", id),
+                 new SqlParameter("@Nome", nome)
+                };
+                return dal.executarReader("select * from Clientes where ID=@id and Nome=@nome", sqlParams);
+            }
+            static public int updateCliente(string id, string nome, string morada, string telefone)
+            {
+                DAL dal = new DAL();
+                SqlParameter[] sqlParams = new SqlParameter[]{
+                new SqlParameter("@id", id),
+                new SqlParameter("@nome", nome),
+                new SqlParameter("@morada", morada),
+                new SqlParameter("@telefone", telefone)
+            };
+                return dal.executarNonQuery("update [Clientes] set [nome]=@nome, [morada]=@morada, [telefone]=@telefone where [id]=@id", sqlParams);
+            }
+            static public int deleteCliente(string id)
+            {
+                DAL dal = new DAL();
+                SqlParameter[] sqlParams = new SqlParameter[]{
+                new SqlParameter("@id", id),
+
+            };
+                return dal.executarNonQuery("Delete From Clientes WHERE[id]=@id", sqlParams);
+            }		
+
+        }
+		
+		public class Imagem
+        {
+            static public object loadpic()
+            {
+                DAL dal = new DAL();
+                SqlParameter[] sqlParams = new SqlParameter[]{
+                new SqlParameter("@id", 1),
+             };
+                return dal.executarScalar("select Img from Imagem where id=@id", sqlParams);
+
+            }
+            static public DataTable Load()
+            {
+                DAL dal = new DAL();
+                return dal.executarReader("select * from Imagem", null);
+            }
+
+            static public int insertImagem(byte[] img)
+            {
+                DAL dal = new DAL();
+                SqlParameter[] sqlParams = new SqlParameter[]{
+                new SqlParameter("@img", img),
+
+           };
+
+                return dal.executarNonQuery("INSERT into Imagem (Img) VALUES(@img)", sqlParams);
+            }
+        }
+    }
+}
---
+++ PT Readify/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("PT Readify")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PT Readify")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("ac1e8373-15cc-4473-9375-3c253ae55288")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
