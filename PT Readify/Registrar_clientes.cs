c4963a52006d197d82247c53cb3182c71cd61414
timsilva916-dev <timsilva916@gmail.com>
2026-03-26 18:18:25 +0000

    teste merge registrar

---
+++ PT Readify/Registrar_clientes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PT_Readify
+{
+    public partial class Registrar_clientes : Form
+    {
+        public Registrar_clientes()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" || textBox2.Text == "" || textBox3.Text == "" || textBox4.Text == ""||textBox5.Text =="")
+            {
+                MessageBox.Show("Preencha todos os campos para registar uma conta.");
+                return;
+            }
+        }
+    }
+}
