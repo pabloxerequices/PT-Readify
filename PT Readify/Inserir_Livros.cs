1dcc72e41b2c1138fbaa2181fb40b54c3a1a5b1d
Rocha <timsilva916@gmail.com>
2026-04-16 18:04:55 +0100

    adicionar livros ns

---
+++ PT Readify/Inserir_Livros.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PT_Readify
+{
+    public partial class Inserir_Livros : Form
+    {
+        public Inserir_Livros()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
