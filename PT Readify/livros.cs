702eb8d00d87e2b453f557c15ed1384b7036b3ee
pabloxerequices <digorodry826@gmail.com>
2026-04-23 18:11:09 +0100

    main menu upgrade(not complete)

--- PT Readify/livros.cs
+++ PT Readify/livros.cs
@@ -26,19 +26,19 @@ namespace PT_Readify
 
         private void livros_Load(object sender, EventArgs e)
         {
-            foreach (DataRow livro in BLL.Livros.Load().Rows)
-            {
-                Panel card = new Panel();
-                card.Size = new Size(150, 220);
-                card.BackColor = Color.White; // Usar o branco gelo da sua paleta
+            //foreach (DataRow livro in BLL.Livros.Load().Rows)
+            //{
+            //    Panel card = new Panel();
+            //    card.Size = new Size(150, 220);
+            //    card.BackColor = Color.White; // Usar o branco gelo da sua paleta
 
-                PictureBox capa = new PictureBox();
-                capa.ImageLocation = livro.CaminhoCapa; // Caminho guardado na BD
-                capa.SizeMode = PictureBoxSizeMode.Zoom;
+            //    PictureBox capa = new PictureBox();
+            //    capa.ImageLocation = livro.CaminhoCapa; // Caminho guardado na BD
+            //    capa.SizeMode = PictureBoxSizeMode.Zoom;
 
-                card.Controls.Add(capa);
-                flowLayoutPanel1.Controls.Add(card);
-            }
+            //    card.Controls.Add(capa);
+            //    flowLayoutPanel1.Controls.Add(card);
+            //}
         }
 
         private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
--- PT Readify/main_menu.Designer.cs
+++ PT Readify/main_menu.Designer.cs
@@ -36,14 +36,22 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.button6 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(26, 444);
+            this.button1.Location = new System.Drawing.Point(12, 953);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(85, 39);
             this.button1.TabIndex = 13;
@@ -53,7 +61,7 @@
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.BackColor = System.Drawing.Color.WhiteSmoke;
             this.panel1.Controls.Add(this.button5);
             this.panel1.Controls.Add(this.button4);
             this.panel1.Controls.Add(this.pictureBox1);
@@ -63,13 +71,13 @@
             this.panel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(275, 518);
+            this.panel1.Size = new System.Drawing.Size(275, 1073);
             this.panel1.TabIndex = 15;
             // 
             // button5
             // 
             this.button5.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button5.Location = new System.Drawing.Point(46, 363);
+            this.button5.Location = new System.Drawing.Point(47, 482);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(176, 54);
             this.button5.TabIndex = 20;
@@ -79,12 +87,13 @@
             // button4
             // 
             this.button4.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(46, 303);
+            this.button4.Location = new System.Drawing.Point(47, 413);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(176, 54);
             this.button4.TabIndex = 19;
             this.button4.Text = "Requisições/Empréstimos";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // pictureBox1
             // 
@@ -99,7 +108,7 @@
             // button3
             // 
             this.button3.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(46, 243);
+            this.button3.Location = new System.Drawing.Point(47, 344);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(176, 54);
             this.button3.TabIndex = 18;
@@ -119,18 +128,86 @@
             this.button2.Text = "Perfil";
             this.button2.UseVisualStyleBackColor = true;
             // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(1022, 372);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(143, 130);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 21;
+            this.pictureBox2.TabStop = false;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(206, 344);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(862, 465);
+            this.flowLayoutPanel1.TabIndex = 23;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(824, 38);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(109, 51);
+            this.button6.TabIndex = 22;
+            this.button6.Text = "Pesquisar";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Moccasin;
+            this.panel2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel2.BackgroundImage")));
+            this.panel2.Controls.Add(this.button7);
+            this.panel2.Controls.Add(this.flowLayoutPanel1);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.button6);
+            this.panel2.Location = new System.Drawing.Point(275, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1794, 1376);
+            this.panel2.TabIndex = 24;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(40, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(197, 73);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "Livros";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(1561, 32);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(50, 47);
+            this.button7.TabIndex = 24;
+            this.button7.Text = "X";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
             // main_menu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.ClientSize = new System.Drawing.Size(1080, 520);
+            this.BackColor = System.Drawing.Color.LightBlue;
+            this.ClientSize = new System.Drawing.Size(1924, 1061);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.panel1);
-            this.IsMdiContainer = true;
             this.Name = "main_menu";
             this.Text = "main_menu";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.main_menu_Load);
             this.panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -144,5 +221,11 @@
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button7;
     }
 }
